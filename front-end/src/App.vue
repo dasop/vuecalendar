ae45c3abec5822986a97627bd7605a95438d76ed
dasop <orangedasop@naver.com>
2020-10-11 20:15:27 +0900

    create scaffold

---
+++ front-end/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-main>
+      <Calendars/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Calendars from './components/Calendars';
+
+export default {
+  name: 'App',
+
+  components: {
+    Calendars,
+  },
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ src/main/java/com/mentoring/VuecalendarsApplication.java
@@ -0,0 +1,13 @@
+package com.mentoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VuecalendarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VuecalendarsApplication.class, args);
+	}
+
+}
